ed203404a658bd61390de37b2a9b3717d39c8007
Akhilesh-Yadav6283 <135110548+Akhilesh-Yadav6283@users.noreply.github.com>
2023-11-20 10:59:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+// function popup()
+// {
+//      alert("First Name:Akhilesh");
+// 	alert("Last Name:Yadav") ;
+// 	 alert("Phone Number:6283724855");
+// 	alert("Email ID:akhiyadav74085@gmail.com");
+// }
+  function submitForm() {
+     const firstName ="Akhilesh";
+     const lastName = "Yadav";
+     const phoneNumber =6283724855;
+     const email = "akhiyadav74085@gmail.com";
+
+     const result =( "First Name: " + firstName + "\nLast Name: " + lastName + "\nPhone Number: " + phoneNumber + "\nEmail ID: " + email);
+
+      alert(result);
+  }
